15090fd3603e788a14affc4118e19d093fb9ff5f
Riyanda Azis Febrian <riyandaazis00@gmail.com>
2025-09-10 17:10:27 +0700

    Updated Management Route and Controller

--- routes/managementRoutes.js
+++ routes/managementRoutes.js
@@ -7,9 +7,24 @@ const {
 } = require("../Controllers/managementController");
 const router = express.Router();
 
-router.get("/getDivisions", getAllDivisions);
-router.get("/getPrograms", getAllPrograms);
-router.get("/getRoles", getAllRoles);
-router.get("/getPositions", getAllPositions);
+router.post("/programs", managementController.addProgram);
+router.post("/divisions", managementController.addDivision);
+router.post("/positions", managementController.addPosition);
+router.post("/roles", managementController.addRole);
+
+router.get("/getPrograms", managementController.getAllPrograms);
+router.get("/getDivisions", managementController.getAllDivisions);
+router.get("/getPositions", managementController.getAllPositions);
+router.get("/getRoles", managementController.getAllRoles);
+
+router.delete("/programs/:id", managementController.deleteProgrambyId);
+router.delete("/divisions/:id", managementController.deleteDivisionbyId);
+router.delete("/positions/:id", managementController.deletePositionbyId);
+router.delete("/roles/:id", managementController.deleteRolebyId);
+
+router.put("/programs/:id", managementController.updateProgrambyId);
+router.put("/divisions/:id", managementController.updateDivisionbyId);
+router.put("/positions/:id", managementController.updatePositionbyId);
+router.put("/roles/:id", managementController.updateRolebyId);
 
 module.exports = router;
